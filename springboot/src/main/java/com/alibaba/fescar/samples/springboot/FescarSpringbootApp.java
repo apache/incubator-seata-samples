38f6edd194189d2a25f310d807f33f22a7fc04ef
slievrly <slievrly@163.com>
2019-01-30 14:25:27 +0800

    add javadoc copyright, fix package, format code style

--- springboot/src/main/java/com/alibaba/fescar/samples/springboot/FescarSpringbootApp.java
+++ springboot/src/main/java/com/alibaba/fescar/samples/springboot/FescarSpringbootApp.java
@@ -1,4 +1,23 @@
-package com.alibaba.fescar.demo;
+/*
+ *  Copyright 1999-2018 Alibaba Group Holding Ltd.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package com.alibaba.fescar.samples.springboot;
+
+import com.alibaba.dubbo.spring.boot.annotation.EnableDubboConfiguration;
+import com.alibaba.fescar.samples.springboot.service.AssignService;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,9 +27,6 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-import com.alibaba.dubbo.spring.boot.annotation.EnableDubboConfiguration;
-import com.alibaba.fescar.demo.service.AssignService;
 
 /**
  * @Description
--- springboot/src/main/java/com/alibaba/fescar/samples/springboot/config/DruidConfiguration.java
+++ springboot/src/main/java/com/alibaba/fescar/samples/springboot/config/DruidConfiguration.java
@@ -1,4 +1,22 @@
-package com.alibaba.fescar.demo.config;
+/*
+ *  Copyright 1999-2018 Alibaba Group Holding Ltd.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package com.alibaba.fescar.samples.springboot.config;
+
+import javax.sql.DataSource;
 
 import com.alibaba.druid.pool.DruidDataSource;
 import com.alibaba.druid.support.http.StatViewServlet;
@@ -12,8 +30,6 @@ import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-
-import javax.sql.DataSource;
 
 @Configuration
 public class DruidConfiguration {
