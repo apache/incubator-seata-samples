72494d37387d1105b1ce5743758370e9ee67c7c9
slievrly <slievrly@163.com>
2019-01-31 10:10:33 +0800

    update copyright

--- dubbo/src/main/java/com/alibaba/fescar/samples/dubbo/starter/DubboStorageServiceStarter.java
+++ dubbo/src/main/java/com/alibaba/fescar/samples/dubbo/starter/DubboStorageServiceStarter.java
@@ -1,6 +1,23 @@
+/*
+ *  Copyright 1999-2018 Alibaba Group Holding Ltd.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package com.alibaba.fescar.samples.dubbo.starter;
 
 import com.alibaba.fescar.samples.dubbo.ApplicationKeeper;
+
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
 
@@ -10,7 +27,7 @@ public class DubboStorageServiceStarter {
      */
     public static void main(String[] args) {
         ClassPathXmlApplicationContext storageContext = new ClassPathXmlApplicationContext(
-                new String[]{"spring/dubbo-storage-service.xml"});
+            new String[]{"spring/dubbo-storage-service.xml"});
         storageContext.getBean("service");
         JdbcTemplate storageJdbcTemplate = (JdbcTemplate) storageContext.getBean("jdbcTemplate");
         storageJdbcTemplate.update("delete from storage_tbl where commodity_code = 'C00321'");
