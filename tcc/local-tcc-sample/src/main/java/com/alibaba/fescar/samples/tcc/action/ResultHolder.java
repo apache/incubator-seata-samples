c7a1f88d17b0f1152ef779f33e3e3a18bf15aba5
zhangthen <zhangsen0706@126.com>
2019-03-07 17:30:12 +0800

    add Local tcc bean demo

---
+++ tcc/local-tcc-sample/src/main/java/com/alibaba/fescar/samples/tcc/action/ResultHolder.java
@@ -0,0 +1,32 @@
+package com.alibaba.fescar.samples.tcc.action;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * @author zhangsen
+ */
+public class ResultHolder {
+
+    private static Map<String, String> actionOneResults = new ConcurrentHashMap<String, String>();
+
+    private static Map<String, String> actionTwoResults = new ConcurrentHashMap<String, String>();
+
+    public static void setActionOneResult(String txId, String result){
+        actionOneResults.put(txId, result);
+    }
+
+    public static String getActionOneResult(String txId){
+        return actionOneResults.get(txId);
+    }
+
+    public static void setActionTwoResult(String txId, String result){
+        actionTwoResults.put(txId, result);
+    }
+
+    public static String getActionTwoResult(String txId){
+        return actionTwoResults.get(txId);
+    }
+
+
+}
