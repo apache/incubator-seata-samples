5a918078f02fce637454c630e5ff6e6fea5a14b2
zhangthen <zhangsen0706@126.com>
2019-03-12 14:03:17 +0800

    fix  sofarpc tcc demo problems

--- tcc/sofarpc-tcc-sample/src/main/java/com/alibaba/fescar/samples/tcc/sofarpc/action/impl/TccActionTwoImpl.java
+++ tcc/sofarpc-tcc-sample/src/main/java/com/alibaba/fescar/samples/tcc/sofarpc/action/impl/TccActionTwoImpl.java
@@ -11,23 +11,23 @@ public class TccActionTwoImpl implements TccActionTwo {
 
     @Override
     public boolean prepare(BusinessActionContext actionContext, String b) {
-        String txId = actionContext.getTxId();
-        System.out.println("TccActionTwo prepare, txId:" + txId);
+        String txId = actionContext.getXid();
+        System.out.println("TccActionTwo prepare, xid:" + txId);
         return true;
     }
 
     @Override
     public boolean commit(BusinessActionContext actionContext) {
-        String txId = actionContext.getTxId();
-        System.out.println("TccActionTwo commit, txId:" + txId);
+        String txId = actionContext.getXid();
+        System.out.println("TccActionTwo commit, xid:" + txId);
         ResultHolder.setActionTwoResult(txId, "T");
         return true;
     }
 
     @Override
     public boolean rollback(BusinessActionContext actionContext) {
-        String txId = actionContext.getTxId();
-        System.out.println("TccActionTwo rollback, txId:" + txId);
+        String txId = actionContext.getXid();
+        System.out.println("TccActionTwo rollback, xid:" + txId);
         ResultHolder.setActionTwoResult(txId, "R");
         return true;
     }
