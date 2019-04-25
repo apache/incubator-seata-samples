678cb31c43213e46cbe355a2c6e5b9550eeb1777
wu <xingfudeshi@vip.qq.com>
2019-04-25 15:31:43 +0800

    update package name.

---
+++ springcloud-jpa-seata/user-service/src/main/java/io/seata/sample/entity/Account.java
@@ -0,0 +1,45 @@
+package io.seata.sample.entity;
+
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "account_tbl")
+@DynamicUpdate
+@DynamicInsert
+public class Account {
+
+    @Id
+    private Long id;
+    private String userId;
+    private BigDecimal money;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public BigDecimal getMoney() {
+        return money;
+    }
+
+    public void setMoney(BigDecimal money) {
+        this.money = money;
+    }
+}
