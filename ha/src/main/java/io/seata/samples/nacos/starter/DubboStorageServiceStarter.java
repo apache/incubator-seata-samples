115c8c3bb5c82764024caf5619e0f8c73c0d71e2
zhangthen <shaohui.zs@alibaba-inc.com>
2019-05-23 15:30:18 +0800

    change seata-* to seata-all & add ha  sample

---
+++ ha/src/main/java/io/seata/samples/nacos/starter/DubboStorageServiceStarter.java
@@ -0,0 +1,41 @@
+/*
+ *  Copyright 1999-2018 Alibaba Group Holding Ltd.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package io.seata.samples.nacos.starter;
+
+import io.seata.samples.nacos.ApplicationKeeper;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+/**
+ * The type Dubbo storage service starter.
+ */
+public class DubboStorageServiceStarter {
+    /**
+     * 1. Storage service is ready . A seller add 100 storage to a sku: C00321
+     *
+     * @param args the input arguments
+     */
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext storageContext = new ClassPathXmlApplicationContext(
+            new String[] {"spring/dubbo-storage-service.xml"});
+        storageContext.getBean("service");
+        JdbcTemplate storageJdbcTemplate = (JdbcTemplate)storageContext.getBean("jdbcTemplate");
+        storageJdbcTemplate.update("delete from storage_tbl where commodity_code = 'C00321'");
+        storageJdbcTemplate.update("insert into storage_tbl(commodity_code, count) values ('C00321', 100)");
+        new ApplicationKeeper(storageContext).keep();
+    }
+}
