b8a5a34bf0d257fc7d6d7fb4aff5ce0799abb436
jimin.jm <jimin.jm@alibaba-inc.com>
2019-06-19 20:14:30 +0800

    add eureka sample
    Signed-off-by: jimin.jm <jimin.jm@alibaba-inc.com>

---
+++ springcloud-eureka-seata/eureka/src/main/java/io/seata/sample/EurekaServerApplication.java
@@ -0,0 +1,17 @@
+package io.seata.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author jimin.jm@alibaba-inc.com
+ * @date 2019/06/19
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
