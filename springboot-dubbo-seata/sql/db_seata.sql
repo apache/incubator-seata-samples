73194139d888e162990cb834d83521d3c299d955
jimin.jm <jimin.jm@alibaba-inc.com>
2019-07-17 12:18:13 +0800

    update undo_log table script
    Signed-off-by: jimin.jm <jimin.jm@alibaba-inc.com>

--- springboot-dubbo-seata/sql/db_seata.sql
+++ springboot-dubbo-seata/sql/db_seata.sql
@@ -76,6 +76,7 @@ CREATE TABLE `undo_log` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `branch_id` bigint(20) NOT NULL,
   `xid` varchar(100) NOT NULL,
+  `context` varchar(128) NOT NULL,
   `rollback_info` longblob NOT NULL,
   `log_status` int(11) NOT NULL,
   `log_created` datetime NOT NULL,
@@ -83,7 +84,7 @@ CREATE TABLE `undo_log` (
   `ext` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `ux_undo_log` (`xid`,`branch_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=94 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of undo_log
