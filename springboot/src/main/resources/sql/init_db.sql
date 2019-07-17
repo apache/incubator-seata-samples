267e4be6c61db013f04b7e60a54a1668c5094082
jimin <slievrly@gmail.com>
2019-07-17 12:22:39 +0800

    update undo_log table script (#177)

--- springboot/src/main/resources/sql/init_db.sql
+++ springboot/src/main/resources/sql/init_db.sql
@@ -40,6 +40,7 @@ CREATE TABLE `undo_log` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `branch_id` bigint(20) NOT NULL,
   `xid` varchar(100) NOT NULL,
+  `context` varchar(128) NOT NULL,
   `rollback_info` longblob NOT NULL,
   `log_status` int(11) NOT NULL,
   `log_created` datetime NOT NULL,
@@ -47,7 +48,7 @@ CREATE TABLE `undo_log` (
   `ext` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `ux_undo_log` (`xid`,`branch_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=159;
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of t_asset
--- springcloud-jpa-seata/sql/all_in_one.sql
+++ springcloud-jpa-seata/sql/all_in_one.sql
@@ -17,20 +17,19 @@ VALUES (1, '1001', 10000);
 INSERT INTO account_tbl (id, user_id, money)
 VALUES (2, '1002', 10000);
 
-CREATE TABLE `undo_log`
-(
-    `id`            BIGINT(20)   NOT NULL AUTO_INCREMENT,
-    `branch_id`     BIGINT(20)   NOT NULL,
-    `xid`           VARCHAR(100) NOT NULL,
-    `rollback_info` LONGBLOB     NOT NULL,
-    `log_status`    INT(11)      NOT NULL,
-    `log_created`   DATETIME     NOT NULL,
-    `log_modified`  DATETIME     NOT NULL,
-    `ext`           VARCHAR(100) DEFAULT NULL,
-    PRIMARY KEY (`id`),
-    UNIQUE KEY `ux_undo_log` (`xid`, `branch_id`)
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8;
+CREATE TABLE `undo_log` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `branch_id` bigint(20) NOT NULL,
+  `xid` varchar(100) NOT NULL,
+  `context` varchar(128) NOT NULL,
+  `rollback_info` longblob NOT NULL,
+  `log_status` int(11) NOT NULL,
+  `log_created` datetime NOT NULL,
+  `log_modified` datetime NOT NULL,
+  `ext` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `ux_undo_log` (`xid`,`branch_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 # Order
 DROP SCHEMA IF EXISTS db_order;
@@ -48,20 +47,19 @@ CREATE TABLE `order_tbl`
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8;
 
-CREATE TABLE `undo_log`
-(
-    `id`            BIGINT(20)   NOT NULL AUTO_INCREMENT,
-    `branch_id`     BIGINT(20)   NOT NULL,
-    `xid`           VARCHAR(100) NOT NULL,
-    `rollback_info` LONGBLOB     NOT NULL,
-    `log_status`    INT(11)      NOT NULL,
-    `log_created`   DATETIME     NOT NULL,
-    `log_modified`  DATETIME     NOT NULL,
-    `ext`           VARCHAR(100) DEFAULT NULL,
-    PRIMARY KEY (`id`),
-    UNIQUE KEY `ux_undo_log` (`xid`, `branch_id`)
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8;
+CREATE TABLE `undo_log` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `branch_id` bigint(20) NOT NULL,
+  `xid` varchar(100) NOT NULL,
+  `context` varchar(128) NOT NULL,
+  `rollback_info` longblob NOT NULL,
+  `log_status` int(11) NOT NULL,
+  `log_created` datetime NOT NULL,
+  `log_modified` datetime NOT NULL,
+  `ext` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `ux_undo_log` (`xid`,`branch_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 # Storage
 DROP SCHEMA IF EXISTS db_storage;
@@ -82,17 +80,16 @@ CREATE TABLE `storage_tbl`
 INSERT INTO storage_tbl (id, commodity_code, count)
 VALUES (1, '2001', 1000);
 
-CREATE TABLE `undo_log`
-(
-    `id`            BIGINT(20)   NOT NULL AUTO_INCREMENT,
-    `branch_id`     BIGINT(20)   NOT NULL,
-    `xid`           VARCHAR(100) NOT NULL,
-    `rollback_info` LONGBLOB     NOT NULL,
-    `log_status`    INT(11)      NOT NULL,
-    `log_created`   DATETIME     NOT NULL,
-    `log_modified`  DATETIME     NOT NULL,
-    `ext`           VARCHAR(100) DEFAULT NULL,
-    PRIMARY KEY (`id`),
-    UNIQUE KEY `ux_undo_log` (`xid`, `branch_id`)
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8;
+CREATE TABLE `undo_log` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `branch_id` bigint(20) NOT NULL,
+  `xid` varchar(100) NOT NULL,
+  `context` varchar(128) NOT NULL,
+  `rollback_info` longblob NOT NULL,
+  `log_status` int(11) NOT NULL,
+  `log_created` datetime NOT NULL,
+  `log_modified` datetime NOT NULL,
+  `ext` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `ux_undo_log` (`xid`,`branch_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
