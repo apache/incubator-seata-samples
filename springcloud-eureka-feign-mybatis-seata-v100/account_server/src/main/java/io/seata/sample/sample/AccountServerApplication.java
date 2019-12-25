40c68fcf7d5143559fc59685af2c0697ed5d14ba
IT云清 <1186355422@qq.com>
2019-12-25 17:06:43 +0800

    add:add sca-starter-seata V1.0.0 sample

---
+++ springcloud-eureka-feign-mybatis-seata-v100/account_server/src/main/java/io/seata/sample/sample/AccountServerApplication.java
@@ -0,0 +1,13 @@
+package io.seata.sample.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountServerApplication.class, args);
+	}
+
+}
