2ec2103325c26d18964dc8fd93690347e4676ea7
奋斗吧少年 <lidong1665@163.com>
2020-01-08 11:54:23 +0800

    seata-spring-boot-starter-samples

---
+++ seata-spring-boot-starter-samples/samples-common/src/main/java/io/seata/samples/integration/common/aop/GlobalExceptionHandler.java
@@ -0,0 +1,46 @@
+package io.seata.samples.integration.common.aop;
+
+import io.seata.samples.integration.common.enums.RspStatusEnum;
+import io.seata.samples.integration.common.exception.DefaultException;
+import io.seata.samples.integration.common.response.ObjectResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @Author: heshouyou
+ * @Description  全局异常处理
+ * @Date Created in 2019/1/9 14:12
+ */
+
+
+@Slf4j
+@ControllerAdvice(basePackages = "com.sinochem.finance.hsy")
+public class GlobalExceptionHandler {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    public ObjectResponse exceptionHandler(Exception e){
+        LOGGER.error("【系统抛出Exception异常】 —— 异常内容如下：{}" , e);
+        ObjectResponse objectResponse = new ObjectResponse<>();
+        objectResponse.setStatus(RspStatusEnum.FAIL.getCode());
+        objectResponse.setMessage(RspStatusEnum.FAIL.getMessage());
+        return objectResponse;
+    }
+
+    @ExceptionHandler(DefaultException.class)
+    @ResponseBody
+    public ObjectResponse defaultException(DefaultException e){
+        LOGGER.error("【系统抛出SinochemException异常】 —— 异常内容如下：{}" , e);
+        ObjectResponse objectResponse = new ObjectResponse<>();
+        objectResponse.setStatus(RspStatusEnum.FAIL.getCode());
+        objectResponse.setMessage(RspStatusEnum.FAIL.getMessage());
+        return objectResponse;
+    }
+}
---
+++ seata-spring-boot-starter-samples/samples-order/src/main/java/io/seata/samples/integration/order/controller/TOrderController.java
@@ -0,0 +1,38 @@
+package io.seata.samples.integration.order.controller;
+
+import io.seata.samples.integration.common.dto.OrderDTO;
+import io.seata.samples.integration.common.response.ObjectResponse;
+import io.seata.samples.integration.order.service.ITOrderService;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ *  订单服务
+ * </p>
+ *
+ * * @author lidong
+ * @since 2019-09-04
+ */
+@RestController
+@RequestMapping("/order")
+@Slf4j
+public class TOrderController {
+
+
+    @Autowired
+    private ITOrderService orderService;
+
+    @PostMapping("/create_order")
+    ObjectResponse<OrderDTO> createOrder(@RequestBody OrderDTO orderDTO){
+        log.info("请求订单微服务：{}",orderDTO.toString());
+        return orderService.createOrder(orderDTO);
+    }
+}
+
