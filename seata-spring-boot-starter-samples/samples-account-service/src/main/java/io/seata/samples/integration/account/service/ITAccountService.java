9efbe8401a8152a4787921b84e8a1db3e452ece6
奋斗吧少年 <lidong1665@163.com>
2020-02-20 12:15:03 +0800

    add seata-spring-boot-starter-samples module (#295)

---
+++ seata-spring-boot-starter-samples/samples-account-service/src/main/java/io/seata/samples/integration/account/service/ITAccountService.java
@@ -0,0 +1,22 @@
+package io.seata.samples.integration.account.service;
+
+import com.baomidou.mybatisplus.service.IService;
+import io.seata.samples.integration.account.entity.TAccount;
+import io.seata.samples.integration.common.dto.AccountDTO;
+import io.seata.samples.integration.common.response.ObjectResponse;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * * @author lidong
+ * @since 2019-09-04
+ */
+public interface ITAccountService extends IService<TAccount> {
+
+    /**
+     * 扣用户钱
+     */
+    ObjectResponse decreaseAccount(AccountDTO accountDTO);
+}
---
+++ seata-spring-boot-starter-samples/samples-business-service/src/main/java/io/seata/samples/integration/call/service/BusinessServiceImpl.java
@@ -0,0 +1,105 @@
+package io.seata.samples.integration.call.service;
+
+import io.seata.core.context.RootContext;
+import io.seata.samples.integration.common.dto.BusinessDTO;
+import io.seata.samples.integration.common.dto.CommodityDTO;
+import io.seata.samples.integration.common.dto.OrderDTO;
+import io.seata.samples.integration.common.dubbo.OrderDubboService;
+import io.seata.samples.integration.common.dubbo.StorageDubboService;
+import io.seata.samples.integration.common.enums.RspStatusEnum;
+import io.seata.samples.integration.common.exception.DefaultException;
+import io.seata.samples.integration.common.response.ObjectResponse;
+import io.seata.spring.annotation.GlobalTransactional;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.dubbo.config.annotation.Reference;
+import org.springframework.stereotype.Service;
+
+/**
+ * @Author: lidong
+ * @Description  Dubbo业务发起方逻辑
+ * @Date Created in 2019/9/5 18:36
+ */
+@Service
+@Slf4j
+public class BusinessServiceImpl implements BusinessService{
+
+    @Reference(version = "1.0.0")
+    private StorageDubboService storageDubboService;
+
+    @Reference(version = "1.0.0")
+    private OrderDubboService orderDubboService;
+
+    boolean flag;
+
+    /**
+     * 处理业务逻辑 正常的业务逻辑
+     * @Param:
+     * @Return:
+     */
+    @GlobalTransactional(timeoutMills = 300000, name = "dubbo-gts-seata-example")
+    @Override
+    public ObjectResponse handleBusiness(BusinessDTO businessDTO) {
+        log.info("开始全局事务，XID = " + RootContext.getXID());
+        ObjectResponse<Object> objectResponse = new ObjectResponse<>();
+        //1、扣减库存
+        CommodityDTO commodityDTO = new CommodityDTO();
+        commodityDTO.setCommodityCode(businessDTO.getCommodityCode());
+        commodityDTO.setCount(businessDTO.getCount());
+        ObjectResponse storageResponse = storageDubboService.decreaseStorage(commodityDTO);
+        //2、创建订单
+        OrderDTO orderDTO = new OrderDTO();
+        orderDTO.setUserId(businessDTO.getUserId());
+        orderDTO.setCommodityCode(businessDTO.getCommodityCode());
+        orderDTO.setOrderCount(businessDTO.getCount());
+        orderDTO.setOrderAmount(businessDTO.getAmount());
+        ObjectResponse<OrderDTO> response = orderDubboService.createOrder(orderDTO);
+
+        if (storageResponse.getStatus() != 200 || response.getStatus() != 200) {
+            throw new DefaultException(RspStatusEnum.FAIL);
+        }
+
+        objectResponse.setStatus(RspStatusEnum.SUCCESS.getCode());
+        objectResponse.setMessage(RspStatusEnum.SUCCESS.getMessage());
+        objectResponse.setData(response.getData());
+        return objectResponse;
+    }
+
+    /**
+     * 出处理业务服务，出现异常回顾
+     *
+     * @param businessDTO
+     * @return
+     */
+    @GlobalTransactional(timeoutMills = 300000, name = "dubbo-gts-seata-example")
+    @Override
+    public ObjectResponse handleBusiness2(BusinessDTO businessDTO) {
+        log.info("开始全局事务，XID = " + RootContext.getXID());
+        ObjectResponse<Object> objectResponse = new ObjectResponse<>();
+        //1、扣减库存
+        CommodityDTO commodityDTO = new CommodityDTO();
+        commodityDTO.setCommodityCode(businessDTO.getCommodityCode());
+        commodityDTO.setCount(businessDTO.getCount());
+        ObjectResponse storageResponse = storageDubboService.decreaseStorage(commodityDTO);
+        //2、创建订单
+        OrderDTO orderDTO = new OrderDTO();
+        orderDTO.setUserId(businessDTO.getUserId());
+        orderDTO.setCommodityCode(businessDTO.getCommodityCode());
+        orderDTO.setOrderCount(businessDTO.getCount());
+        orderDTO.setOrderAmount(businessDTO.getAmount());
+        ObjectResponse<OrderDTO> response = orderDubboService.createOrder(orderDTO);
+
+//        打开注释测试事务发生异常后，全局回滚功能
+        if (!flag) {
+            throw new RuntimeException("测试抛异常后，分布式事务回滚！");
+        }
+
+        if (storageResponse.getStatus() != 200 || response.getStatus() != 200) {
+            throw new DefaultException(RspStatusEnum.FAIL);
+        }
+
+        objectResponse.setStatus(RspStatusEnum.SUCCESS.getCode());
+        objectResponse.setMessage(RspStatusEnum.SUCCESS.getMessage());
+        objectResponse.setData(response.getData());
+        return objectResponse;
+    }
+}
