f0cd1ab863d506cbacef010ab2ca3183c7d281f1
xuanyi <haiqiang.shq@alibaba-inc.com>
2020-03-23 23:46:01 +0800

    fix data source configuration issue

---
+++ seata-xa/order-xa/src/main/java/io/seata/sample/OrderXADataSourceConfiguration.java
@@ -0,0 +1,30 @@
+package io.seata.sample;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import io.seata.rm.datasource.DataSourceProxy;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class OrderXADataSourceConfiguration {
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DruidDataSource druidDataSource() {
+        return new DruidDataSource();
+    }
+
+    @Bean("dataSourceProxy")
+    public DataSource dataSource(DruidDataSource druidDataSource) {
+        return new DataSourceProxy(druidDataSource);
+    }
+
+    @Bean("jdbcTemplate")
+    public JdbcTemplate jdbcTemplate(DataSource dataSourceProxy) {
+        return new JdbcTemplate(dataSourceProxy);
+    }
+}
