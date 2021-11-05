13665b63dfb58de3b2ef8ad1d1e107c01b9381cf
HankDevelop <kuliuht@sina.com>
2021-11-05 12:22:46 +0800

    add jdbcTemplate batchUpdate Examples (#338)

--- dubbo/src/main/java/io/seata/samples/dubbo/service/impl/BusinessServiceImpl.java
+++ dubbo/src/main/java/io/seata/samples/dubbo/service/impl/BusinessServiceImpl.java
@@ -42,6 +42,8 @@ public class BusinessServiceImpl implements BusinessService {
     public void purchase(String userId, String commodityCode, int orderCount) {
         LOGGER.info("purchase begin ... xid: " + RootContext.getXID());
         storageService.deduct(commodityCode, orderCount);
+        // double deduct
+        storageService.batchDeduct(commodityCode, orderCount);
         orderService.create(userId, commodityCode, orderCount);
         //throw new RuntimeException("xxx");
 
