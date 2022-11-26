5d62a961272edf3cb152d7504c36897c1b8aaa11
FUNKYE <364176773@qq.com>
2022-11-26 15:43:30 +0800

    new branch

--- nutzboot-dubbo-fescar/nutzboot-dubbo-fescar-common/src/main/java/io/nutz/demo/dubbo/rpc/service/AccountService.java
+++
@@ -1,9 +0,0 @@
-package io.nutz.demo.dubbo.rpc.service;
-
-public interface AccountService {
-
-    /**
-     * debit balance of user's account
-     */
-    void debit(String userId, int money);
-}
--- nutzboot-dubbo-fescar/nutzboot-dubbo-fescar-common/src/main/java/io/nutz/demo/dubbo/rpc/service/StockService.java
+++
@@ -1,9 +0,0 @@
-package io.nutz.demo.dubbo.rpc.service;
-
-public interface StockService {
-
-    /**
-     * deduct stock count
-     */
-    void deduct(String commodityCode, int count);
-}
