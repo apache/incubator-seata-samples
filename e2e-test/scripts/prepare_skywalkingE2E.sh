7ebeff53b0d267a8397f5f8cde941ca813a50c66
Jingliu Xiong <928124786@qq.com>
2024-05-11 12:34:22 +0800

    fix: fix e2e framework to right version

--- e2e-test/scripts/prepare_skywalkingE2E.sh
+++ e2e-test/scripts/prepare_skywalkingE2E.sh
@@ -1,1 +1,2 @@
-go install github.com/apache/skywalking-infra-e2e/cmd/e2e@latest
+# use skywalking-infra-e2e v1.3.0
+go install github.com/apache/skywalking-infra-e2e/cmd/e2e@1485ae03f0ad90496ad7626a5ae4a6a73a1f6296
