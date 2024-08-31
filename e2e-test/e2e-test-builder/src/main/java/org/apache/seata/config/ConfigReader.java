8bd5db55edb368f80eaccbf1bf15af75ac24cc26
jimin <slievrly@163.com>
2024-08-31 22:40:51 +0800

    optimize: upgrade some dependencies (#686)

--- e2e-test/e2e-test-builder/src/main/java/org/apache/seata/config/ConfigReader.java
+++ e2e-test/e2e-test-builder/src/main/java/org/apache/seata/config/ConfigReader.java
@@ -16,20 +16,24 @@
  */
 package org.apache.seata.config;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+
 import org.apache.seata.model.E2EConfig;
 import org.apache.seata.model.E2EWrapper;
+import org.yaml.snakeyaml.LoaderOptions;
 import org.yaml.snakeyaml.Yaml;
 import org.yaml.snakeyaml.constructor.Constructor;
-
-import java.io.*;
-import java.nio.file.Files;
 
 /**
  * @author jingliu_xiong@foxmail.com
  */
 public class ConfigReader {
     public static E2EConfig readConfig(File configFile) throws IOException {
-        Yaml yaml = new Yaml(new Constructor(E2EWrapper.class));
+        LoaderOptions loaderOptions = new LoaderOptions();
+        Yaml yaml = new Yaml(new Constructor(E2EWrapper.class, loaderOptions));
         E2EWrapper e2EWrapper = new E2EWrapper();
         InputStream inputStream = Files.newInputStream(configFile.toPath());
         // 从YAML文件读取数据并转换为Java对象
