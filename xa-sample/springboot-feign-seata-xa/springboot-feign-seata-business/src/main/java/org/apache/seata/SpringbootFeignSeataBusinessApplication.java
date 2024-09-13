96d7946285cc67679063808a8df6a042b985053d
Jingliu Xiong <928124786@qq.com>
2024-09-13 22:57:01 +0800

    feature: up seata dependency to 2.1.0

--- xa-sample/springboot-feign-seata-xa/springboot-feign-seata-business/src/main/java/org/apache/seata/SpringbootFeignSeataBusinessApplication.java
+++ xa-sample/springboot-feign-seata-xa/springboot-feign-seata-business/src/main/java/org/apache/seata/SpringbootFeignSeataBusinessApplication.java
@@ -16,12 +16,14 @@
  */
 package org.apache.seata;
 
+import io.seata.spring.annotation.datasource.EnableAutoDataSourceProxy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableAutoDataSourceProxy(dataSourceProxyMode = "XA")
 public class SpringbootFeignSeataBusinessApplication  {
 
     public static void main(String[] args) {
