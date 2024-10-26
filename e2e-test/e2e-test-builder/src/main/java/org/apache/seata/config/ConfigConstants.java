b952ea74477f40d5204495830a8883d5451d662c
Jingliu Xiong <928124786@qq.com>
2024-10-26 14:18:24 +0800

    feat: test spring-seata-dubbo-saga e2e case

--- e2e-test/e2e-test-builder/src/main/java/org/apache/seata/config/ConfigConstants.java
+++ e2e-test/e2e-test-builder/src/main/java/org/apache/seata/config/ConfigConstants.java
@@ -23,7 +23,7 @@ public class ConfigConstants {
     public static final String IMAGE_DIR = "tmp/images";
     public static final String SCENE_DIR = "tmp/scene-test";
     public static final String REPLACE_DIR = "tmp/replace";
-    public static final String SEATA_E2E_FILE = "seata-e2e.yaml";
+    public static final String SEATA_E2E_FILE = "seata-e2e1.yaml";
     public static final String E2E_FILES = "e2e-files";
     public static final String COMPOSE_FILE = "docker-compose.yaml";
     public static final String SKY_WALKING_E2E_FILE = "e2e.yaml";
