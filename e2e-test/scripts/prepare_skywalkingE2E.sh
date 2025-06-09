b91223860d85a6ab33830a1d287ca8c53d74d297
jimin <slievrly@163.com>
2025-06-09 19:44:05 +0800

    add license header (#708)
    * add license header
    
    Signed-off-by: slievrly <slievrly@163.com>
    
    * add license header
    
    Signed-off-by: slievrly <slievrly@163.com>
    
    ---------
    
    Signed-off-by: slievrly <slievrly@163.com>

--- e2e-test/scripts/prepare_skywalkingE2E.sh
+++ e2e-test/scripts/prepare_skywalkingE2E.sh
@@ -1,2 +1,19 @@
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 # use skywalking-infra-e2e v1.3.0
 go install github.com/apache/skywalking-infra-e2e/cmd/e2e@1485ae03f0ad90496ad7626a5ae4a6a73a1f6296
