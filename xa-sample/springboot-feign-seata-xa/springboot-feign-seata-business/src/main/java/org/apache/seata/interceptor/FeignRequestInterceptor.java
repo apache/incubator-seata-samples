dcce531f5fec93c40305f4fa26246c52fc6d0d32
shukai <525718210@qq.com>
2025-10-13 15:07:22 +0800

    optimize: Annotations adjusted to English (#718)

--- xa-sample/springboot-feign-seata-xa/springboot-feign-seata-business/src/main/java/org/apache/seata/interceptor/FeignRequestInterceptor.java
+++ xa-sample/springboot-feign-seata-xa/springboot-feign-seata-business/src/main/java/org/apache/seata/interceptor/FeignRequestInterceptor.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.seata.interceptor;
 
 import org.apache.seata.core.context.RootContext;
@@ -11,7 +27,7 @@ public class FeignRequestInterceptor implements RequestInterceptor {
 
     @Override
     public void apply(RequestTemplate template) {
-        // 从全局事务上下文中获取 TX_XID 并添加到 Feign 请求的 Header 中
+        // Obtain the TX_XID from the global transaction context and add it to the Feign request header
         String xid = RootContext.getXID();
         if (xid != null) {
             template.header(RootContext.KEY_XID, xid);
